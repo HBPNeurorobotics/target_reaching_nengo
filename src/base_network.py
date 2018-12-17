04417dc90baa308ee506b1258ef38f1944744f7a
Juan Camilo Vasquez Tieck <tieck@fzi.de>
2018-12-17 19:01:03 +0100

    imports cleanup

--- src/base_network.py
+++ src/base_network.py
@@ -1,12 +1,13 @@
 import nengo
-import rospy
 import numpy as np
-from std_msgs.msg import Float64
-# import rhythmic
-import voluntary
+
 # import reflex
 import feedback
-from std_msgs.msg import String
+import rospy
+# import rhythmic
+import voluntary
+from std_msgs.msg import Float64, String
+
 
 class Base_network():
     def __init__(self, voluntary_joints = [],  rhythmic_joints = [], stim = None, use_stim = True):
@@ -21,13 +22,13 @@ class Base_network():
             for j in range(len(self.all_motions[i])):
                 self.all_joints.append(self.all_motions[i][j])
         unique_joints = sorted(set(self.all_joints), key=self.all_joints.index)
-        self._joints_pub= [[] for y in range(2)] 
+        self._joints_pub= [[] for y in range(2)]
         for i in unique_joints:
             self._joints_pub[0].append(i)
             self._joints_pub[1].append(rospy.Publisher(i, Float64, queue_size=1))
-        self.result = [] 
-    
-     
+        self.result = []
+
+
     def publish_topic(self, t, x):
         if self.use_stim:
             for i in range(len(self._joints_pub[1])):
@@ -35,7 +36,7 @@ class Base_network():
         else:  # FOR TR
             for i in range(len(self._joints_pub[1])):
                 # NEAR FAR
-                if self._joints_pub[0][i] == '/HoLLiE/hollie_real_left_arm_3_joint/cmd_pos': 
+                if self._joints_pub[0][i] == '/HoLLiE/hollie_real_left_arm_3_joint/cmd_pos':
                    # self._joints_pub[1][i].publish(-1.1)
                     if abs(self.error[0]) >= 1:
                         self._joints_pub[1][i].publish(x[i])
@@ -44,36 +45,36 @@ class Base_network():
                     #self._joints_pub[1][i].publish(1.0)
                     if abs(self.error[1]) >= 1:
                         self._joints_pub[1][i].publish(x[i])
-                else: 
+                else:
                     #self._joints_pub[1][i].publish(0.0)
                     if abs(self.error[2]) >= 1:
                         self._joints_pub[1][i].publish(x[i])
-        
+
 
 
     def set_start_pos(self, joint, value):
         pub= rospy.Publisher(joint, Float64, queue_size=1)
         pub.publish(value)
-    
+
     def set_errorFN(self, x):
         self.error[0] = x
         return x
-    
+
     def set_errorHR(self, x):
         self.error[1] = x
         return x
-    
+
     def set_errorLR(self, x):
         self.error[2] = x
         return x
-        
+
 
     def get_network(self, label):
         net = nengo.Network(label=label)
         with net:
             def blend(x):
                 # TODO : wenn mehr als 1 joint doppelt?
-                res=[] 
+                res=[]
                 duplette=[]
                 tmp=[]
                 for i in range(len(x)):
@@ -84,25 +85,25 @@ class Base_network():
                 val = duplette[0][0] +  duplette[1][0] / 2
                 #######  BLEN ERROR #####
                 ##### DEBUG #####
-                #val = duplette[1][0] 
+                #val = duplette[1][0]
                 res.append([val, duplette[0][1]])
                 for i in range(len(self._joints_pub[0])):
                     for j in range(len(res)):
                         if self._joints_pub[0][i] == res[j][1]:
                             tmp.append(res[j][0])
-                return tmp 
-                
-                
+                return tmp
+
+
             if self.use_stim:
                 if self.stim is None: net.stim = nengo.Node(np.zeros(self.slider_nr))
                 else: net.stim = nengo.Node(self.stim, label = 'vol __ a __ b')
-            
-         #   net.input = nengo.Node(self.set_error) 
+
+         #   net.input = nengo.Node(self.set_error)
            # net.f_u= nengo.Ensemble(n_neurons=800, dimensions=len(self.all_joints), radius=14)   #orginal
             net.f_u= nengo.Ensemble(n_neurons=100, dimensions=len(self.all_joints), radius=2, neuron_type=nengo.Direct(), label ='g(f(u))')   #direct
             #net.f_u= nengo.Ensemble(n_neurons=800, dimensions=len(self.all_joints), radius=2)  #ohne direct
-            
-            if len(self._joints_pub[0]) is not len(self.all_joints):       
+
+            if len(self._joints_pub[0]) is not len(self.all_joints):
                 #net.f_u_blended = nengo.Ensemble(n_neurons=700, dimensions=len(set(self._joints_pub[0])), radius=12)   #orginal
                 net.f_u_blended = nengo.Ensemble(n_neurons=100, dimensions=len(set(self._joints_pub[0])), radius=2, neuron_type=nengo.Direct(), label= 'g(f(u)) blended')    #direct
                 #net.f_u_blended = nengo.Ensemble(n_neurons=800, dimensions=len(set(self._joints_pub[0])), radius=2)     #ohne direct
