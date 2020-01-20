1b39c7444f1f1a4f351f52a8a79c3627611920b4
Daniel Azanov <azanov@fzi.de>
2020-01-20 18:48:32 +0100

    nengo model building and running without gui

--- nodes/start_nengo_gui.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import nengo_gui
-import rospkg
-import os
-
-file_path = rospkg.RosPack().get_path('target_reaching_nengo')+'/nodes/main_TR_CL_node.py'
-command = 'nengo ' + file_path
-os.system(command)
