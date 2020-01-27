b237899e359727392a99a015a46747816a268c91
Daniel Azanov <azanov@fzi.de>
2020-01-27 14:41:49 +0100

    spawning iiwa14

--- nodes/spawn_model.py
+++ nodes/spawn_model.py
@@ -44,32 +44,29 @@ model_sdf_xml = """
 class SpawnGazeboSDFModel:
     def __init__(self, model_name, sdf_xml):
         self._model_name = model_name
-        #self._sdf_spawn_proxy = rospy.ServiceProxy('/gazebo/spawn_sdf_entity',
-                                               #SpawnEntity, persistent=True)
-        self._sdf_spawn_proxy = rospy.ServiceProxy('/gazebo/spawn_urdf_entity',
+        self._sdf_spawn_proxy = rospy.ServiceProxy('/gazebo/spawn_sdf_entity',
                                                SpawnEntity, persistent=True)
         self._model_msg = SpawnEntityRequest()
         self._model_msg.entity_name = self._model_name
         self._sdf_xml = sdf_xml
         self._model_colors = ['Green']  #, 'Red', 'Blue', 'Orange', 'Yellow', 'Purple', 'Turquoise']
-        #self._model_msg.entity_xml = sdf_xml.format(model_name=model_name, color=np.random.choice(self._model_colors, size=1)[0])
-        self._model_msg.entity_xml = sdf_xml
+        self._model_msg.entity_xml = sdf_xml.format(model_name=model_name, color=np.random.choice(self._model_colors, size=1)[0])
         self._model_msg.initial_pose.position.x = 0.0
         self._model_msg.initial_pose.position.y = 0.0
         self._model_msg.initial_pose.position.z = 0.55
         self._model_msg.reference_frame = "world"
 
     def spawn(self):
+        self._model_msg.entity_xml = self._sdf_xml.format(model_name=model_name, color=np.random.choice(self._model_colors, size=1)[0])
         self._sdf_spawn_proxy(self._model_msg)
         return 'success'
 
 def main(argv=None):
     rospy.init_node("SpawnGazeboSDFModel")
-    #rospack = rospkg.RosPack()
-    #model_path = rospack.get_path('target_reaching_experiments') + '/gazebo_models/target_reaching_subject.sdf'
-    #with open(model_path, "r") as model_file:
-        #product_xml = model_file.read()
-    product_xml = rospy.get_param('/robot_description')
+    rospack = rospkg.RosPack()
+    model_path = rospack.get_path('iiwa_description') + '/sdf/iiwa14.sdf'
+    with open(model_path, "r") as model_file:
+        product_xml = model_file.read()
     spawn_sdf = SpawnGazeboSDFModel("my_model", product_xml)
     rospy.loginfo("SpawnGazeboSDFModel initialized")
     spawn_sdf.spawn()
